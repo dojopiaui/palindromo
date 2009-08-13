da4f45d1ea8c9d7226ebfab4a317b5618fdca3a3
Cleiton Francisco <cleitonfco@gmail.com>
2009-08-13 12:32:48 -0300

    Colocando mais um palíndromo nos testes

--- spec/palindrome_spec.rb
+++ spec/palindrome_spec.rb
@@ -3,7 +3,8 @@ require 'lib/palindrome'
 describe String do
   describe "palindrome" do
     [ 'ovo', 'ana', 'a rua laura', 'A Rua laura', 'Ze de Lima. Rua Laura, Mil e Dez',
-      'A droga do dote! é todo da gorda?', 'Amarga-me a droga, a gorda é magra Má'].each do |palindrome|
+      'A droga do dote! é todo da gorda?', 'Amarga-me a droga, a gorda é magra Má', 
+      'O pó de cocaína mata maníaco cedo, pô!'].each do |palindrome|
       it "#{palindrome} deve ser verdadeiro" do
         palindrome.palindrome?.should be_true
       end
